fix weighting scheme

data_methods.py
@@ -5,7 +5,7 @@ import healpy as hp
 from PathSegment import PathSegment
 
 
-def run_file(file, nside, energy_falloff=2.7):
+def run_file(file, nside):
     print("Processing: " + file)
     data1 = np.load(file)
 
@@ -23,8 +23,6 @@ def run_file(file, nside, energy_falloff=2.7):
             if p_last.status < 0:
                 break  # If particle failed, don't include it in data
 
-            weight = 1 / np.power(p_first.p / 1000, energy_falloff - 1)
-
             data_array.append((hp.vec2pix(nside, p_first.px, p_first.py, p_first.pz) + 1, weight))
             data_array.append((-hp.vec2pix(nside, p_last.x, p_last.y, p_last.z) - 1, weight))
 
@@ -35,7 +33,7 @@ def run_file(file, nside, energy_falloff=2.7):
     return data_array
 
 
-def reweigh_file(file, nside, weights):
+def reweigh_file(file, nside, weights, energy_falloff=2.7):
     print("Reweighing: " + file)
     data1 = np.load(file)
 
@@ -53,9 +51,11 @@ def reweigh_file(file, nside, weights):
             if p_last.status < 0:
                 break  # If particle failed, don't include it in data
 
+            weight = np.power(p_first.p / 1000, energy_falloff - 1)
+
             pix = hp.vec2pix(nside, p_first.px, p_first.py, p_first.pz)
             fin_pix = hp.vec2pix(nside, p_last.x, p_last.y, p_last.z)
-            data_array.append((pix, weights[fin_pix]))
+            data_array.append((pix, weights[fin_pix] * weight))
 
         except:
             sys.excepthook(*sys.exc_info())