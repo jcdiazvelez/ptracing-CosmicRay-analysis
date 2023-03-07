440d84a6eba6896fa37caf4cc01108301d980570
Freddie Floydd <ftf23@cam.ac.uk>
2023-03-07 15:10:05 +0000

    Edit sandbox.py

--- src/sandbox.py
+++ src/sandbox.py
@@ -4,10 +4,6 @@ import numpy as np
 import healpy as hp
 from matplotlib import pyplot as plt
 
-from data_methods import create_bin_sizes, bin_particles, create_reweighed_sky_maps, \
-    create_time_maps, get_reweighed_particles, rotate_map, rotate_map_sim
-from statistical_methods import perform_kolmogorov_smirnov
-
 # Read in particles
 particles_filename = '../data/nside=32.npz'
 particles_file = np.load(particles_filename)
@@ -26,7 +22,7 @@ for item in particle_array:
     initial_map[initial_pixel] += 1
 
 plt.set_cmap('coolwarm')
-hp.visufunc.mollview(initial_map * np.sum(initial_map) / npix,
+hp.visufunc.mollview(initial_map / (np.sum(initial_map) / npix),
                      # title=f'Relative intensity at edge of simulation for ' + "{0:.3g}".format(lower_limit) +
                      # title="{0:.3g}".format(lower_limit) +
                      #      ' TeV < E < ' + "{0:.3g}".format(upper_limit) + ' TeV',
